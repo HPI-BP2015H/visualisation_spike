3d8d3491c6b8ea1275bfa489845999f36538868c
Lennard Wolf <lennardwolf@live.de>
2015-12-03 16:16:25 +0100

    workInProgress

--- travisAPIcrawlFunctions.js
+++ travisAPIcrawlFunctions.js
@@ -24,5 +24,28 @@ function getBuilds() {
 
 function getAllCommitsWithDate(){
   var builds = getBuilds();
-  console.log(builds);
+  var array = [['ID', 'Weekday']];
+  for(var i = 0; i < builds.length; i++) {
+    a = [''];
+    a.push(builds[i].commit.id);
+    console.log(builds[i].commit.committed_at);
+    a.push(new Date(builds[i].commit.committed_at).getDay());
+    array.push(a);
+  }
+
+  console.log(array);
+}
+
+function getWeekday(aDateInt){
+  var weekday = new Array(7);
+  weekday[0]=  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  return weekday[aDateInt];
+
 }
