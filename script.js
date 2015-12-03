bcecc77e415b560ccee1cec033dd7b7525397350
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2015-12-03 14:04:29 +0100

    Mocking stuff.

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+function generateCharts() {
+  generateStackedAreaChart();
+}
+
+function generateStackedAreaChart() {
+  // Just a mock-up right now.
+  google.load("visualization", "1.0", {packages:["corechart"]});
+  google.setOnLoadCallback(function() {
+    var container = document.getElementById('stackedAreaChart');
+    var chart = new google.visualization.AreaChart(container);
+    var data = google.visualization.arrayToDataTable([
+      ["Build", "Fail", "Pass"],
+      [    "1",      7,     83],
+      [    "2",      8,     82],
+      [    "3",      0,     90],
+      [    "4",     15,     90],
+      [    "5",      7,     98]
+    ]);
+    var options = {
+      isStacked: true,
+      height: 400,
+      legend: {
+        position: "top",
+        maxLines: 3
+      },
+      hAxis: {
+        title: "Builds"
+      },
+      vAxis: {
+        title: "Tests",
+        minValue: 0
+      }
+    };
+    chart.draw(data, options);
+  });
+}
+
 function printXML() {
-  /*var content = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?> <testsuite name=\"BaselineOfSWTDemo\" tests=\"1\" failures=\"0\" errors=\"0\" time=\"0.0\"> <testcase classname=\"SWTDemo.Tests.SWTDemoTest\" name=\"testAnotherValue\" time=\"0.0\">     </testcase><testcase classname=\"SWTDemo.Tests.SWTDemoTest\" name=\"testValue\" time=\"0.0\">     </testcase>    <system-out><![CDATA[]]></system-out>    <system-err><![CDATA[]]></system-err> </testsuite>";
-  var parser = new DOMParser();
-  var tree = parser.parseFromString(content, "text/xml");
-
-  console.log("baseline: " + tree.getElementsByTagName("testcase")[0].getAttribute("classname"));
-  console.log("tests: " + tree.documentElement.getAttribute("tests"));
-  console.log("failures: " + tree.documentElement.getAttribute("failures"));
-  console.log("errors: " + tree.documentElement.getAttribute("errors"));
-  console.log("time: " + tree.documentElement.getAttribute("time"));*/
-
   $.ajaxSetup({async: true});
   var slug = "HPI-BP2015H%2FSWT-Demo"
 
@@ -35,22 +62,17 @@ function printXML() {
 function processBuilds(builds) {
   for(var i = 0; i < builds.length; i++) {
     for(var j = 0; j < builds[i].jobs.length; j++) {
-      jQuery.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "http://api.travis-ci.org/jobs/" + builds[i].jobs[j].id,
-        success: function (data, status, jqXHR) {
+      var target = "https://api.travis-ci.org/jobs/" + builds[i].jobs[j].id + "/log.txt?deansi=true";
+      jQuery.get(
+        target,
+        function(data) {
           console.log("[DONE] get logs");
-          document.getElementsByTagName("body")[0].innerHTML += data.id + ":<br/><br/>";
-          document.getElementsByTagName("body")[0].innerHTML += data.log;
-          document.getElementsByTagName("body")[0].innerHTML += "<br/><br/><br/>";
+          document.getElementsByTagName("body")[0].innerHTML += "JOB:<br/><br/>\n";
+          document.getElementsByTagName("body")[0].innerHTML += data;
+          document.getElementsByTagName("body")[0].innerHTML += "<br/><br/><br/>\n";
         },
-        error: function (jqXHR, status) {
-          console.log("[FAILED] get logs");
-          console.log(jqXHR);
-          console.log(status);
-        }
-      });
+        'text'
+      );
     }
   }
 }
