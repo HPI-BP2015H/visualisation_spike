1620a2bbac6624a4180cd3742c670765cc013400
Christopher Weyand <christopher.weyand@student.hpi.de>
2015-12-07 16:27:53 +0100

    testCase

---
+++ JUnitTestCase.js
@@ -0,0 +1,23 @@
+var JUnitBranch = function(xml){
+	
+	this.time = getTime();
+	this.className = setClassName();
+	this.testName = setTestName();
+	this.status = setStatus();
+
+	function setClassName(){
+	//TODO
+	return 0;
+	}
+	
+	function setTestName(){
+	//TODO 
+	return 0;
+	}
+
+	function setStatus(){
+	//TODO
+	return "error";
+	}
+
+}
