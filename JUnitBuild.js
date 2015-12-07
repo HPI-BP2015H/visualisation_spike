d15882e60ca09159cbac78b53cbae5d0ee7d4f98
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2015-12-07 15:19:55 +0100

    Add JUnitJob stub.

--- JUnitBuild.js
+++ JUnitBuild.js
@@ -1,27 +1,24 @@
 var JUnitBuild = function(id) {
   this.id = id;
 
+  this.jobs = getJobs();
+
   // Retrieve values via REST API.
   this.commitTime    = getCommitTime();
   this.committerName = getCommitterName();
   this.status        = getStatus();
 
-  // Retrieve and parse JUnitXML.
-  var jUnitXML = getXML();
-  var jParser  = new DOMParser();
-  var jDOM     = jParser.parseFromString(jUnitXML, "text/xml");
-
-  // Generate values from JUnitXML.
+  // Aggregate values from JUnitJobs.
   this.time          = getBuildTime(jDOM);
   this.testcaseCount = getTestcaseCount(jDOM);
   this.failCount     = getFailCount(jDOM);
   this.passCount     = getPassCount(jDOM);
   this.errorCount    = getErrorCount(jDOM);
 
-  /*
-   * TODO:
-   * Implement substructure for jobs, suites, (<-- oxford comma) and testcases.
-   */
+  function getJobs() {
+    // TODO: Implement.
+    return [];
+  }
 
   function getCommitTime() {
     // TODO: Implement.
@@ -36,15 +33,6 @@ var JUnitBuild = function(id) {
   function getStatus() {
     // TODO: Implement.
     return "green"; // Maybe use boolean instead?
-  }
-
-  function getXML() {
-    // TODO: Implement.
-    // 1. get log
-    // 2. extract xml from log
-    // (or mock xml first)
-    var mock = '<?xml version="1.0" encoding="UTF-8"?><testsuite name="#(\'BaselineOfSWTDemo\') Test Suite" tests="1" failures="0" errors="2" time="0.0"><testcase classname="SWTDemo.Tests.SWTDemoTest" name="testAnotherValue" time="0.0"><error type="TestFailure" message="Assertion failed">SWTDemoTest(TestCase)>>signalFailure:\nSWTDemoTest(TestCase)>>assert:\nSWTDemoTest>>testAnotherValue\nSWTDemoTest(TestCase)>>performTest\n</error></testcase><testcase classname="SWTDemo.Tests.SWTDemoTest" name="testValue" time="0.0"><error type="TestFailure" message="Assertion failed">SWTDemoTest(TestCase)>>signalFailure:\nSWTDemoTest(TestCase)>>assert:\nSWTDemoTest>>testValue\nSWTDemoTest(TestCase)>>performTest\n</error></testcase><system-out><![CDATA[]]></system-out><system-err><![CDATA[]]></system-err></testsuite>';
-    return mock;
   }
 
   function getBuildTime(jDOM) {
@@ -71,9 +59,4 @@ var JUnitBuild = function(id) {
     // TODO: Implement.
     return 0;
   }
-}
-
-function jUnitBuildTest() {
-  var testBuild = new JUnitBuild(94661528);
-  console.log(testBuild);
 }
---
+++ JUnitJob.js
@@ -0,0 +1,74 @@
+var JUnitJob = function(id) {
+  this.id = id;
+
+  // Retrieve values via REST API.
+  this.status = getStatus();
+
+  // Retrieve and parse JUnitXML.
+  var jUnitXML = getXML();
+  var jParser  = new DOMParser();
+  var jDOM     = jParser.parseFromString(jUnitXML, "text/xml");
+
+  // Generate values from JUnitXML.
+  this.os            = getOS();
+  this.env           = getEnv();
+  this.time          = getJobTime(jDOM);
+  this.testcaseCount = getTestcaseCount(jDOM);
+  this.failCount     = getFailCount(jDOM);
+  this.passCount     = getPassCount(jDOM);
+  this.errorCount    = getErrorCount(jDOM);
+
+  function getStatus() {
+    // TODO: Implement.
+    return "green";
+  }
+
+  function getOS() {
+    // TODO: Implement.
+    return "dummy";
+  }
+
+  function getEnv() {
+    // TODO: Implement.
+    return "dummy";
+  }
+
+  function getJobTime(jDOM) {
+    // TODO: Implement.
+    return 0;
+  }
+
+  function getTestcaseCount(jDOM) {
+    // TODO: Implement.
+    return 0;
+  }
+
+  function getFailCount(jDOM) {
+    // TODO: Implement.
+    return 0;
+  }
+
+  function getPassCount(jDOM) {
+    // TODO: Implement.
+    return 0;
+  }
+
+  function getErrorCount(jDOM) {
+    // TODO: Implement.
+    return 0;
+  }
+
+  function getXML() {
+    // TODO: Implement.
+    // 1. get log
+    // 2. extract xml from log
+    // (or mock xml first)
+    var mock = '<?xml version="1.0" encoding="UTF-8"?><testsuite name="#(\'BaselineOfSWTDemo\') Test Suite" tests="1" failures="0" errors="2" time="0.0"><testcase classname="SWTDemo.Tests.SWTDemoTest" name="testAnotherValue" time="0.0"><error type="TestFailure" message="Assertion failed">SWTDemoTest(TestCase)>>signalFailure:\nSWTDemoTest(TestCase)>>assert:\nSWTDemoTest>>testAnotherValue\nSWTDemoTest(TestCase)>>performTest\n</error></testcase><testcase classname="SWTDemo.Tests.SWTDemoTest" name="testValue" time="0.0"><error type="TestFailure" message="Assertion failed">SWTDemoTest(TestCase)>>signalFailure:\nSWTDemoTest(TestCase)>>assert:\nSWTDemoTest>>testValue\nSWTDemoTest(TestCase)>>performTest\n</error></testcase><system-out><![CDATA[]]></system-out><system-err><![CDATA[]]></system-err></testsuite>';
+    return mock;
+  }
+}
+
+function jUnitJobTest() {
+  var testJob = new JUnitJob(94661529);
+  console.log(testJob);
+}
