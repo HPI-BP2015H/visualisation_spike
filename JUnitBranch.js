6f569dd71dd0cd0bc1d29b9717ec544cc4d6b5a6
Lennard Wolf <lennardwolf@live.de>
2015-12-07 14:15:48 +0100

    getBuilds in Repo

--- JUnitBranch.js
+++ JUnitBranch.js
@@ -1,19 +1,15 @@
-var JUnitBranch = function(rawData) {
-
-  
-
+var JUnitBranch = function(rawData, builds, slug) {
   this.slug = slug;
-  this.name = name;
+  this.name = rawData.name;
+  this.builds = builds;
+  getBuilds();
 
-  var buildIDs = getBuildIDs();
-  var builds = [];
 
-  for(var i = 0; i < buildIDs.length; i++) {
-    builds.push(new JUnitBuild(buildIDs[i]));
-  }
-
-  function getBuildIDs() {
-    // TODO: Implement.
-    return [];
+  function getBuilds() {
+    var apiPath = "https://api.travis-ci.org/v3/repo/" + this.slug + "/branches";
+    var result = getResultFromAPIPath(apiPath).builds;
+    for each (build in result) {
+      this.builds.push(new JUnitBuild(build, this.slug));
+    }
   }
 }
--- JUnitRepository.js
+++ JUnitRepository.js
@@ -1,16 +1,31 @@
 var JUnitRepository = function(slug) {
   this.slug = slug;
+  this.branches = [];
+  this.allBuilds = getAllBuilds();
+  getBranches();
 
-  var branches = getBranches();
-
-  /*for(var i = 0; i < branchNames.length; i++) {
-    branches.push(new JUnitBranch(slug, branchNames[i]));
-  }*/
 
   function getBranches() {
     var apiPath = "https://api.travis-ci.org/v3/repo/" + this.slug + "/branches";
     var result = getResultFromAPIPath(apiPath).branches;
-    console.log(result);
-    return [];
+    for each (branch in result) {
+      var branchBuilds = getBuildsForBranch(branch.name);
+      this.branches.push(new JUnitBranch(branch, branchBuilds, this.slug));
+    }
+  }
+
+  function getAllBuilds() {
+    var apiPath = "https://api.travis-ci.org/v3/repo/" + this.slug + "/builds";
+    return getResultFromAPIPath(apiPath).builds;
+  }
+
+  function getBuildsForBranch(aBranchName) {
+    var b = [];
+    for each (build in this.allBuilds) {
+      if (build.branch.name == aBranchName) {
+        b.push(build);
+      }
+    }
+    return b;
   }
 }
