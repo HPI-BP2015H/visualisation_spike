7c1ec2cad4e72c3a50647021b5d16c50485bcea0
Steffen Kötte <steffen.koette@gmail.com>
2015-12-07 16:50:30 +0100

    refactor JUnitBuild and JUnitRepository

--- JUnitRepository.js
+++ JUnitRepository.js
@@ -1,38 +1,24 @@
 var JUnitRepository = function(slug) {
-  //methods
-
-  this.getBranches = function() {
-    var apiPath = "https://api.travis-ci.org/v3/repo/" + this.slug + "/branches";
-    var result = getResultFromTravisAPI(apiPath).branches;
-    for (var i = 0; i < result.length; i++) {
-      var branchBuilds = this.getBuildsForBranch(result[i].name);
-      this.branches.push(new JUnitBranch(result[i], branchBuilds, this.slug));
-    }
-  }
-
-  this.getBuildsForBranch = function(aBranchName) {
-    var b = [];
-    for (var i = 0; i < this.allTravisBuilds.length; i++) {
-      if (this.allTravisBuilds[i].branch.name == aBranchName) {
-        b.push(this.allTravisBuilds[i]);
-      }
-    }
-    return b;
-  }
 
   //variables
 
   this.slug = slug;
   this.branches = [];
-  this.allTravisBuilds = getAllTravisBuilds();
-  this.getBranches();
+
+  // init
+
+  this.branches = getAllBranches(this.slug);
 
   //private
 
-  function getAllTravisBuilds() {
-    var apiPath = "https://api.travis-ci.org/v3/repo/" + this.slug + "/builds";
-    return getResultFromTravisAPI(apiPath).builds;
+  function getAllBranches(slug) {
+    var apiPath = "https://api.travis-ci.org/v3/repo/" + slug + "/branches";
+    var branches = [];
+    var travisBranches = getResultFromTravisAPI(apiPath).branches;
+    for (var i = 0; i < travisBranches.length; i++) {
+      branches.push(new JUnitBranch(slug, travisBranches[i].name));
+    }
+    return branches;
   }
-
 
 }
